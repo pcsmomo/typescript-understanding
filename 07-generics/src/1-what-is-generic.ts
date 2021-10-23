ad5d68ebbbd6f5c72dcb4b49263e44e86e4946e7
pcsmomo <dongwoo_333@hanmail.net>
2021-10-23 19:49:03 +1100

    104. Another Generic Function

--- 07-generics/src/1-what-is-generic.ts
+++ 07-generics/src/1-what-is-generic.ts
@@ -1,7 +1,8 @@
 const names: Array<string> = []; // string[] // 100% same
 // name[0].split(' ');
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+const promise: Promise<number> = new Promise((resolve) => {
   setTimeout(() => {
     // resolve('This is done!');
     resolve(10);
