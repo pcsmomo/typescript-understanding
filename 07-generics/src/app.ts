829c3c9dfa7071901773b62df8fc4e383f0afd18
pcsmomo <dongwoo_333@hanmail.net>
2021-10-23 18:45:30 +1100

    101. Built-in Generics & What are Generics?

--- 07-generics/src/app.ts
+++ 07-generics/src/app.ts
@@ -1,1 +1,13 @@
-// Code goes here!
+const names: Array<string> = []; // string[] // 100% same
+// name[0].split(' ');
+
+const promise: Promise<number> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve('This is done!');
+    resolve(10);
+  }, 2000);
+});
+
+// promise.then((data) => {
+//   data.split(' ');
+// });
