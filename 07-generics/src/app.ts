2044903084dfce7a75f62ddc7d99c557f035d475
pcsmomo <dongwoo_333@hanmail.net>
2021-10-23 19:53:35 +1100

    separate javascript

--- 07-generics/src/app.ts
+++
@@ -1,8 +0,0 @@
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return 'Value: ' + obj[key];
-}
-
-console.log(extractAndConvert({ name: 'Max' }, 'name'));
