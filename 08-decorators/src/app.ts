6fdad97fe8c62b9ed28c92f9778e7b03f66e8b8a
pcsmomo <dongwoo_333@hanmail.net>
2021-10-24 08:44:17 +1100

    114. Working with Decorator Factories

--- 08-decorators/src/app.ts
+++ 08-decorators/src/app.ts
@@ -1,10 +1,33 @@
-// It is being created first before pers instance created.
-function Logger(constructor: Function) {
-  console.log('Logging...');
-  console.log(constructor);
+// // ****************************
+// // 1. A First Class Decorator
+// // It is being created first before pers instance created.
+// function Logger(constructor: Function) {
+//   console.log('Logging...');
+//   console.log(constructor);
+// }
+
+// @Logger
+// class Person {
+//   name = 'Max';
+
+//   constructor() {
+//     console.log('Creating person object...');
+//   }
+// }
+
+// const pers = new Person();
+// console.log(pers);
+
+// ****************************
+// 2. Working with Decorator Factories
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+@Logger('LOGGING - PERSON')
 class Person {
   name = 'Max';
 
@@ -14,5 +37,4 @@ class Person {
 }
 
 const pers = new Person();
-
 console.log(pers);
