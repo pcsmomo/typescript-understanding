19158c45b90648110914f26f198410c1799b2579
pcsmomo <dongwoo_333@hanmail.net>
2021-10-23 18:57:34 +1100

    102. Creating a Generic Function

--- 07-generics/src/app.ts
+++ 07-generics/src/app.ts
@@ -1,1 +1,17 @@
-//
+// function merge(objA: object, objB: object) {
+//   return Object.assign(objA, objB);
+// }
+
+// const mergeObj = merge({ name: 'Max' }, { age: 30 });
+// mergeObj.age; // Type script wouldn't know about age
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+// const mergedObj = merge<{ name: string; hobbies: string[] }, { age: number }>(
+//   { name: 'Max', hobbies: ['Sports'] },
+//   { age: 30 }
+// );
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj);
