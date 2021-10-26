cdb67204ee3e8713c21a643a8b3bc807e7aa3f4f
pcsmomo <dongwoo_333@hanmail.net>
2021-10-26 16:48:13 +1100

    139. Adding Inheritance & Generics: STATE

--- 09-drag-drop-project/src/app.ts
+++ 09-drag-drop-project/src/app.ts
@@ -14,14 +14,25 @@ class Project {
 }
 
 // Project State Management
-type Listener = (items: Project[]) => void;
+// type Listener = (items: Project[]) => void;
+type Listener<T> = (items: T[]) => void;
 
-class ProjectState {
-  private listeners: Listener[] = [];
+class State<T> {
+  protected listeners: Listener<T>[] = [];
+
+  // Subscription pattern
+  addListener(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn);
+  }
+}
+
+class ProjectState extends State<Project> {
   private projects: Project[] = [];
   private static instance: ProjectState;
 
-  private constructor() {}
+  private constructor() {
+    super();
+  }
 
   static getInstance() {
     if (this.instance) {
@@ -29,11 +40,6 @@ class ProjectState {
     }
     this.instance = new ProjectState();
     return this.instance;
-  }
-
-  // Subscription pattern
-  addListener(listenerFn: Listener) {
-    this.listeners.push(listenerFn);
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
