abf9d2ff0162e9a1428357ca344b089aa7751108
pcsmomo <dongwoo_333@hanmail.net>
2021-10-28 18:20:39 +1100

    171. No Types Needed: class-transformer: INTRO

--- 12-3rd-party-libraries/2-typescript-libraries/src/app.ts
+++ 12-3rd-party-libraries/2-typescript-libraries/src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+import { Product } from './product.model';
+
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 10.99 },
+];
+
+// const p1 = new Product('A Book', 12.99);
+// console.log(p1.getInformation());
+
+const loadedProducts = products.map((prod) => {
+  return new Product(prod.title, prod.price);
+});
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
