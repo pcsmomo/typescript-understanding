bbeac894e995451d68f69b796f84a1fcd26df01f
pcsmomo <dongwoo_333@hanmail.net>
2021-10-30 14:31:05 +1100

    200. Adding Middleware & Types

--- 15-node-express/src/app.ts
+++ 15-node-express/src/app.ts
@@ -1,5 +1,15 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import { json } from 'stream/consumers';
+
+import todoRoutes from './routes/todos';
 
 const app = express();
+
+app.use('/todos', todoRoutes);
+
+// Error Handling Middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
